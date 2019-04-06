a1b5f773ffdc9b491db77eb391fd5e41cb5f0ba9
temprory <beishengren@126.com>
2019-04-06 13:46:24 +0800

    -

--- coroutine.go
+++ coroutine.go
@@ -105,11 +105,11 @@ type LinkTask struct {
 	ch     chan interface{}
 }
 
-func (task *LinkTask) Done(data interface{}) {
+func (task *LinkTask) Next(data interface{}) {
 	task.ch <- data
 }
 
-func (task *LinkTask) Wait() interface{} {
+func (task *LinkTask) Pre() interface{} {
 	if task.pre != nil {
 		return <-task.pre
 	}
--- test/corslink.go
+++ test/corslink.go
@@ -15,6 +15,8 @@ func main() {
 	time.Sleep(time.Second)
 	wg := sync.WaitGroup{}
 	wg.Add(1)
+
+	allData := []interface{}{}
 	for i := 0; i < 50; i++ {
 		idx := i
 		delay := time.Second + time.Duration(rand.Intn(10))*time.Second/10
@@ -22,19 +24,25 @@ func main() {
 		cl.Go(func(task *util.LinkTask) {
 			//fmt.Println("begin", idx, delay)
 			time.Sleep(delay)
-			task.Wait()
+			preData := task.Pre()
+			fmt.Println("preData:", preData)
 			fmt.Println("end", idx)
-			task.Done(idx)
+			currData := idx
+			task.Next(currData)
+
+			allData = append(allData, currData)
 			if idx == 49 {
 				defer wg.Done()
 				cl.StopAsync()
 				time.Sleep(time.Second)
-
 			}
 
 		})
 
 	}
 	wg.Wait()
+	for i, v := range allData {
+		fmt.Println("--- ", i, v)
+	}
 	fmt.Println("cornum 2: ", runtime.NumGoroutine())
 }
